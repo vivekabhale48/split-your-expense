09d1bdfa0349b1b9e69cb1f2c6c60bbce18fedda
vivekabhale48 <vivek.abhale48@gmail.com>
2025-01-15 00:57:51 +0530

    added the calc button

--- src/components/BillPageSection.jsx
+++ src/components/BillPageSection.jsx
@@ -1,10 +1,24 @@
 import { useSelector } from "react-redux"
 import { ParticularBillSection } from "./ParticularBillSection";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const BillPageSection = () => {
 
     const data = useSelector((state) => state.homePageContent.data);
+    const [creditors, setCreditors] = useState([])
+    const [debitors, setDebitors] = useState([])
+
+    const calculateTheExpense = () => {
+        data?.members.forEach((member) => {
+            if(member?.netBalance > 0) {
+                setCreditors((prev) => [...prev, member])
+            }
+            else {
+                setDebitors((prev) => [...prev, member])
+            }
+        })
+        
+    }
 
     return (
         <div className="text-white">
@@ -16,6 +30,12 @@ export const BillPageSection = () => {
                         <ParticularBillSection key={index} memberDetail={memberDetail} />
                     ))
                 }
+            </div>
+
+            <div className="flex items-center justify-center px-4 mt-10 mb-10">
+                <button onClick={calculateTheExpense} className="bg-[#1e4b94] text-white py-3 px-3 rounded-md font-bold hover:bg-white hover:text-[#1e4b94] transition cursor-pointer">
+                    Calculate The Expense
+                </button>
             </div>
         </div>
     )
